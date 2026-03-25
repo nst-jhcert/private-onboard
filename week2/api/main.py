14244a0e658b4271f1f1e55c8fbb4634fbfa221d
nst-jhcert <jhcert@naraspace.com>
2026-03-25 16:44:19 +0900

    add: Add python main implementation from week1

---
+++ week2/api/main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+
+api = FastAPI(
+    title="ONBOARD-WEEK2",
+    description="""
+## ONBOARD-WEEK2
+### 목표
+- 컨테이너로 분리된 메모 앱을 구현합니다.
+### 기술스택
+- FastAPI
+- Docker
+- PostgreSQL
+- Redis
+""", 
+    version="1.0.0",
+    docs_url="/docs",
+    redoc_url="/redoc",
+)
+
+@api.get("/", summary="서버의 상태를 반환합니다.", tags=["WEEK2"])
+async def root():
+    return "OK"
