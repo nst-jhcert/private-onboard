0c887195ba87e3f4ccfd4176d1c5aa2b70ade016
nst-jhcert <jhcert@naraspace.com>
2026-03-26 12:48:46 +0900

    add: Add CSP parser test to entrypoint

--- week3/csp/entrypoint.sh
+++ week3/csp/entrypoint.sh
@@ -2,6 +2,9 @@
 cd /app/libcsp
 python3 ./examples/buildall.py
 
+cd /app/parser
+make test
+
 chmod 600 /root/.ssh/authorized_keys
 chown root:root /root/.ssh/authorized_keys
 exec /usr/sbin/sshd -D
